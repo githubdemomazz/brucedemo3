efff69e36c785edc299fa7dc6ac398a41c6c83df
githubdemomazz <mazzucca12@gmail.com>
2022-03-08 22:11:47 -0500

    Updates

---
+++ js/main.js
@@ -0,0 +1,50 @@
+
+$(document).ready(function(){
+  $("p").click(function(){
+    $(this).hide();
+  });
+});
+
+function abcd(){
+	document.getElementById("getsanimage").style.backgroundImage = "url('housewithdriveway.jpg')";
+	document.getElementById("getsanimage").style.height = "950px";
+	document.getElementById("boxes3").style.paddingTop = "20px";
+	document.getElementById("getsanimage").style.backgroundPosition = "-250px -225px";
+	document.getElementById("getsanimage").style.backgroundRepeat = "no-repeat";
+};
+function cablets(){
+	window.location.href = "./heh.html";
+}
+function qbcd(){
+	document.getElementById("mapperboxsd1").style.paddingBottom = "75%";
+	document.getElementById("mapperboxsd1").style.height = "0";
+	document.getElementById("mapperboxsd2").style.paddingBottom = "75%";
+	document.getElementById("mapperboxsd2").style.height = "0";
+	document.getElementById("mapperboxs").style.visibility = "visible";
+	document.getElementById("mapperboxs").style.width = "auto";
+	document.getElementById("mapperboxs").style.height = "auto";
+	//document.getElementById("mapperboxs").style.paddingTop = "20px";
+	document.getElementById("bbqribs").style.marginTop = "20px";
+
+};
+function abracadabra(){
+	document.getElementById("bbqribs").style.width = "100%";
+};
+function dddd(){
+	//document.getElementById("vijaboxd1").style.paddingBottom = "75%";
+	//document.getElementById("vijaboxd1").style.height = "0";
+	document.getElementById("vijaboxd2").style.padding = "15px";
+	document.getElementById("vidjaboxs").style.visibility = "visible";
+	document.getElementById("vidjaboxs").style.width = "auto";
+	document.getElementById("vidjaboxs").style.height = "auto";
+	document.getElementById("vidjaboxs").style.padding = "20px";
+	document.getElementById("vidjaboxs").style.margin = "auto";
+	document.getElementById("vidjaboxs").style.marginBottom = "20px";
+	document.getElementById("vidjaboxs").style.border = "2px black solid";
+}
+
+
+
+
+
+
